94f51d9720498ec6be541fb99d4dec3563e03237
vishwas1143 <vishwasburra23143@gmail.com>
2025-07-25 08:25:27 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,83 +4,65 @@ import axios from "axios";
 import "./../styles/App.css";
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [hasFetched, setHasFetched] = useState(false); // Needed to detect initial state
 
-  const getData = useCallback(() => {
+  const getUsers = () => {
     setLoading(true);
     axios
       .get("https://reqres.in/api/users")
       .then((res) => {
-        setData(res.data.data || []);
+        setUsers(res.data.data);
+        setHasFetched(true);
       })
       .catch((err) => {
-        console.error("Error fetching data:", err);
-        setData([]);
+        setUsers([]);
+        setHasFetched(true);
       })
       .finally(() => {
         setLoading(false);
       });
-  }, []);
+  };
 
   return (
     <div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h2>OUTPUT</h2>
-        <p>Blue Whales</p>
-        <button
-          style={{ backgroundColor: "#15778a" }}
-          className="btn"
-          onClick={getData}
-        >
-          Get User List
-        </button>
-      </div>
+      <button className="btn" onClick={getUsers}>
+        Get User List
+      </button>
 
-      <div>
-        {loading ? (
-          <p>Loading users...</p>
-        ) : (
-          <table style={{ width: "100vw", border: "0" }}>
-            <thead style={{ backgroundColor: "black", color: "white" }}>
-              <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Avatar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.length > 0 ? (
-                data.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.first_name}</td>
-                    <td>{user.last_name}</td>
-                    <td>{user.email}</td>
-                    <td>
-                      <img
-                        src={user.avatar}
-                        alt="Avatar"
-                        style={{
-                          width: "100px",
-                          aspectRatio: "1/1",
-                          objectFit: "cover",
-                        }}
-                      />
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan={4} style={{ textAlign: "center" }}>
-                    No data found
+      {loading && <p>Loading users...</p>}
+
+      {!loading && hasFetched && (
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length > 0 ? (
+              users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <img src={user.avatar} alt="avatar" />
                   </td>
                 </tr>
-              )}
-            </tbody>
-          </table>
-        )}
-      </div>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={4}>No data found</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
