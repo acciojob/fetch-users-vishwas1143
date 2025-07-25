84ab0911005510cc86c473cc1185f95dc3b94c6d
vishwas1143 <vishwasburra23143@gmail.com>
2025-07-25 08:22:36 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,28 @@ import "./../styles/App.css";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const getData = useCallback(() => {
-    try {
-      axios
-        .get("https://reqres.in/api/users", {
-          headers: {
-            "x-api-key": "reqres-free-v1",
-          },
-        })
+  const [loading, setLoading] = useState(false);
 
-        .then((d) => setData(d.data.data));
-    } catch (e) {
-      console.error(e);
-    }
+  const getData = useCallback(() => {
+    setLoading(true);
+    axios
+      .get("https://reqres.in/api/users")
+      .then((res) => {
+        setData(res.data.data || []);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setData([]);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
-  console.log(data);
+
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h2>OUTPUT</h2>
         <p>Blue Whales</p>
         <button
           style={{ backgroundColor: "#15778a" }}
@@ -32,44 +36,50 @@ const App = () => {
           Get User List
         </button>
       </div>
+
       <div>
-        <table style={{ width: "100vw", border: "0" }}>
-          <thead style={{ backgroundColor: "black", color: "white" }}>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Avatar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* {id: 1, email: 'george.bluth@reqres.in', first_name: 'George', last_name: 'Bluth', avatar: 'https://reqres.in/img/faces/1-image.jpg'} */}
-            {data.length !== 0 ? (
-              data?.map((e) => {
-                return (
-                  <tr>
-                    <td>{e.first_name}</td>
-                    <td>{e.last_name}</td>
-                    <td>{e.email}</td>
+        {loading ? (
+          <p>Loading users...</p>
+        ) : (
+          <table style={{ width: "100vw", border: "0" }}>
+            <thead style={{ backgroundColor: "black", color: "white" }}>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Avatar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 ? (
+                data.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.email}</td>
                     <td>
                       <img
+                        src={user.avatar}
+                        alt="Avatar"
                         style={{
                           width: "100px",
                           aspectRatio: "1/1",
                           objectFit: "cover",
                         }}
-                        src={e.avatar}
-                        alt="Image"
                       />
                     </td>
                   </tr>
-                );
-              })
-            ) : (
-              <p>No data found</p>
-            )}
-          </tbody>
-        </table>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={4} style={{ textAlign: "center" }}>
+                    No data found
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
